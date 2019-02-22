7123a94a202aad3fa11ead94287d241b587de49b
Flavio Galvao <flaviogalvao0912@gmail.com>
2019-02-22 15:26:53 -0300

    Primeira versão do AcheiTI

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <!-- Caso encontre outra keyword e queira adicionar para o projeto fique a vontade, keywords são para o sistema de procura do google ex: tecnico em informática, ele vai procurar 'tecnico' 'em' 'informatica' no seu banco de dados, tendo assim a oportunidade de ficar no topo da pesquisa. -->
+        <meta name="keywords" content="Emprego, TI, Estágio, Desenvolvedor, Manutenção, Técnico em informatica, Técnico em desenvolvimento de Sistemas, Analista, Desenvolvimento web, Desenvolvimento de Software, Oportunidade, Estágio em Informatica">
+        <!-- Descrição que fica logo abaixo do link principal no google. -->
+        <meta name="description" content="Empresa especializada em oportunidades de Estágio na área de Técnologia da Informação.">
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/mainCss.css">
+        
+        <!-- Scripts -->
+        <script type="text/javascript" src="js/script.js"></script>
+        
+        <!-- Titulo -->
+        <title>AcheiTI - Estágios relacionados a area de informática</title>
+        
+    </head>
+    <body>
+       
+    </body>
+
+</html>
