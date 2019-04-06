4d84f2d56e66e3ca76b64158a9045d465f14f777
Flávio Galvão <flaviogalvao0912@gmail.com>
2019-04-06 00:05:49 -0300

    versão 1.0

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <head>
         
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="icon" type="image/png" href="imagens/etcfavicon.png" /><!--Usando faviconIcon na Aba do URL-->
+        <link rel="icon" type="image/png" href="" /><!--Usando faviconIcon na Aba do URL-->
         <link rel="stylesheet" type="text/css" href="css/estilo.css">
         <!-- Bootstrap -->
         
